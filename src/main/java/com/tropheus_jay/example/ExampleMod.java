cb2f1393a2ba1732241d1388a25b6dfaeb49ea2b
Tropheus Jay <jverrellijr@icloud.com>
2021-08-20 20:36:40 -0400

    h, but again

--- src/main/java/com/tropheus_jay/example/ExampleMod.java
+++ src/main/java/com/tropheus_jay/example/ExampleMod.java
@@ -1,14 +1,9 @@
-package com.fabricmc.example;
+package com.tropheus_jay.example;
 
 import net.fabricmc.api.ModInitializer;
 
 public class ExampleMod implements ModInitializer {
 	@Override
 	public void onInitialize() {
-		// This code runs as soon as Minecraft is in a mod-load-ready state.
-		// However, some things (like resources) may still be uninitialized.
-		// Proceed with mild caution.
-
-		System.out.println("Hello Fabric world!");
 	}
 }
