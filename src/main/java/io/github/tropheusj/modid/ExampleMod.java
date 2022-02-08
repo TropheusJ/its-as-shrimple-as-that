665533f4b5f8a4b0fa65ba2922e6672f3e8e9058
TropheusJ <jverrellijr@icloud.com>
2022-02-07 20:12:22 -0500

    updates and stuff and things

--- src/main/java/io/github/tropheusj/modid/ExampleMod.java
+++ src/main/java/io/github/tropheusj/modid/ExampleMod.java
@@ -4,8 +4,12 @@ import net.fabricmc.api.ModInitializer;
 
 import net.minecraft.resources.ResourceLocation;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class ExampleMod implements ModInitializer {
 	public static final String ID = "modid";
+	public static final Logger LOGGER = LoggerFactory.getLogger(ID);
 
 	@Override
 	public void onInitialize() {
