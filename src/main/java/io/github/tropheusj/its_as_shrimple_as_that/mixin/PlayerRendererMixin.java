a4b7f77606f1b7f6821b5f00d5c4a2eba7226205
TropheusJ <jverrellijr@icloud.com>
2025-03-02 22:08:02 -0500

    fix missing backfaces

--- src/main/java/io/github/tropheusj/its_as_shrimple_as_that/mixin/PlayerRendererMixin.java
+++ src/main/java/io/github/tropheusj/its_as_shrimple_as_that/mixin/PlayerRendererMixin.java
@@ -1,12 +1,5 @@
 package io.github.tropheusj.its_as_shrimple_as_that.mixin;
 
-import com.mojang.math.Axis;
-
-import io.github.tropheusj.its_as_shrimple_as_that.ItsAsShrimpleAsThat;
-import io.github.tropheusj.its_as_shrimple_as_that.entity.render.ShrimpModel;
-
-import io.github.tropheusj.its_as_shrimple_as_that.entity.render.ShrimpRenderer;
-
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -15,7 +8,11 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import com.mojang.blaze3d.vertex.PoseStack;
 import com.mojang.blaze3d.vertex.VertexConsumer;
+import com.mojang.math.Axis;
 
+import io.github.tropheusj.its_as_shrimple_as_that.ItsAsShrimpleAsThat;
+import io.github.tropheusj.its_as_shrimple_as_that.entity.render.ShrimpModel;
+import io.github.tropheusj.its_as_shrimple_as_that.entity.render.ShrimpRenderer;
 import net.minecraft.client.player.AbstractClientPlayer;
 import net.minecraft.client.renderer.MultiBufferSource;
 import net.minecraft.client.renderer.RenderType;
@@ -45,7 +42,7 @@ public class PlayerRendererMixin {
 			matrices.scale(-1, -1, 1);
 			matrices.translate(0, -1.5, 0);
 			matrices.mulPose(Axis.YP.rotationDegrees(player.getYRot() + 180));
-			VertexConsumer buffer = buffers.getBuffer(RenderType.entityCutout(ShrimpRenderer.TEXTURE));
+			VertexConsumer buffer = buffers.getBuffer(RenderType.entityCutoutNoCull(ShrimpRenderer.TEXTURE));
 			this.shrimpModel.renderToBuffer(matrices, buffer, i, OverlayTexture.NO_OVERLAY, 0xFFFFFFFF);
 			matrices.popPose();
 			ci.cancel();
